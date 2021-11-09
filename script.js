61d79625d6e19f292947560e9196c2d8616e2fba
Taggartjoey9 <87246513+Taggartjoey9@users.noreply.github.com>
2021-11-09 18:56:42 -0500

    Added functions

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 let gameChoices = ['Rock', 'Paper', 'Scissors']
 
-//let playerSelection = prompt('Rock, Paper, Scissors?');
+let playerSelection = prompt('Rock, Paper, Scissors?');
 
 let computerSelection = gameChoices[Math.floor(Math.random() * gameChoices.length)];
 
 
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'Rock' && computerSelection === 'Rock') {
-        console.log('You have tied, play again')
+    if (playerSelection === computerSelection) {
+        console.log('Its a tie')
     }
-}
+        else if (
+            (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+            (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
+            (playerSelection === 'Paper' && computerSelection === 'Rock')
+        )
+            console.log('Player has won')
+            
+        
+            else (
+                (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
+                (computerSelection ==='Scissors' && playerSelection === 'Paper') ||
+                (computerSelection === 'Paper' && playerSelection === 'Rock')
+            )
+                console.log('Computer has won')
+            
+            }
+
+
+
